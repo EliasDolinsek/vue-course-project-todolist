4dace7523640fed4099597e5b5ed720e6311d182
eld <elias.dolinsek@gmail.com>
2022-09-02 23:23:57 +0200

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,49 @@
-<script setup></script>
+<script setup>
+import { v4 as uuidv4 } from "uuid";
+import { ref } from "vue";
 
-<template></template>
+const todoItems = ref([
+  {
+    id: uuidv4(),
+    taskName: "First Todo Item",
+    description: "The description for the first Todo Item",
+    dueDate: "2023-01-01",
+    done: false,
+  },
+  {
+    id: uuidv4(),
+    taskName: "Second Todo Item",
+    description: "The description for the second Todo Item",
+    dueDate: "2023-02-01",
+    done: false,
+  },
+]);
+</script>
 
-<style scoped></style>
+<template>
+  <header class="colored-header"></header>
+  <main>
+    <h1 id="title">Todolist</h1>
+    <p style="color: white">Todo Items: {{ todoItems }}</p>
+  </main>
+</template>
+
+<style scoped>
+.colored-header {
+  min-height: 5vh;
+  min-width: 100vw;
+  background: var(--background-light);
+}
+
+#title {
+  font-size: 2rem;
+  font-weight: 600;
+  color: #fff;
+}
+
+@media (min-width: 600px) {
+  main {
+    padding: 0 20vw;
+  }
+}
+</style>
