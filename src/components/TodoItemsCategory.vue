032c11c85c6a2108e408591ea4310082ee9c3965
eld <elias.dolinsek@gmail.com>
2022-09-03 19:48:12 +0200

    Add TodoItemsCategory

---
+++ src/components/TodoItemsCategory.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template></template>
+
+<style scoped>
+.category-title {
+  color: var(--text-color-light);
+  font-weight: 600;
+}
+
+.todo-item {
+  margin-bottom: 16px;
+}
+</style>
