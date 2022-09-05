e0d9c5446a54286a81d8ca1410cf766003ec8604
eld <elias.dolinsek@gmail.com>
2022-09-05 10:50:10 +0200

    Add TodoItemForm

---
+++ src/components/TodoItemForm.vue
@@ -0,0 +1,84 @@
+<script setup>
+</script>
+
+<template>
+  <form class="form-container">
+    <input
+      type="text"
+      placeholder="Title"
+      class="input-title item-input"
+      required
+    />
+    <div class="icon-form-input-container">
+      <span class="material-symbols-outlined icon">text_snippet</span>
+      <input
+        type="text"
+        placeholder="Description"
+        class="item-input item-input-expanded"
+      />
+    </div>
+    <div class="icon-form-input-container">
+      <span class="material-symbols-outlined icon">today</span>
+      <input type="date" class="item-input" />
+    </div>
+    <div class="form-actions-container">
+    </div>
+  </form>
+</template>
+
+<style scoped>
+.item-input {
+  border: none;
+  background: transparent;
+  outline: none;
+  color: var(--text-color-light);
+}
+
+.item-input-expanded {
+  width: 100%;
+}
+
+.item-input[type="date"]::-webkit-inner-spin-button,
+.item-input[type="date"]::-webkit-calendar-picker-indicator {
+  display: none;
+  -webkit-appearance: none;
+}
+
+.input-title {
+  font-size: 1.1rem;
+}
+
+.icon-form-input-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  margin-top: 8px;
+}
+
+.form-actions-container {
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  gap: 8px;
+
+  margin-top: 16px;
+}
+
+.icon-form-input-container > span {
+  margin-right: 8px;
+  font-size: 100%;
+}
+
+.form-container {
+  color: white;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  width: 100%;
+}
+
+.button-remove-date {
+  margin-top: 1px;
+  color: var(--danger-color);
+}
+</style>
