c963c2b4b1cc726c9640014db8481df37bd03bdc
eld <elias.dolinsek@gmail.com>
2022-09-06 22:33:11 +0200

    Add TodoItem

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,57 @@
+<script setup>
+// TODO: 3. Erstelle ein modelValue-prop (type=Object, required=true)
+</script>
+
+<template>
+  <div class="todo-item-container">
+    <!--TODO: 4. Erstelle einen Paragraph, welche das modelValue-Objekt ausgiebt, mit style="color: white"-->
+  </div>
+</template>
+
+<style scoped>
+.todo-item-container {
+  background: var(--background-light);
+  display: flex;
+  flex-direction: row;
+  border-radius: 10px;
+}
+
+.todo-item-title {
+  color: var(--text-color-light);
+  font-weight: 600;
+  margin-bottom: 0;
+}
+
+.todo-item-title-done {
+  text-decoration: line-through;
+}
+
+.todo-item-subtitle {
+  color: var(--text-color-dark);
+  font-weight: 600;
+  font-size: 0.8rem;
+  margin-top: 4px;
+  margin-bottom: 0;
+}
+
+.actions-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 16px;
+  padding-top: 12px;
+  padding-bottom: 16px;
+}
+
+.todo-item-check-button {
+  margin-top: 18px;
+  margin-left: 16px;
+  margin-right: 16px;
+}
+
+.todo-item-form-container {
+  margin-top: 8px;
+  margin-bottom: 8px;
+  width: 100%;
+}
+</style>
