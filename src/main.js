bfdf515f5ed5b5aa7935b1d423e0c2d457769c98
eld <elias.dolinsek@gmail.com>
2023-06-23 15:16:56 +0200

    Updated instructions

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ import App from "./App.vue";
 import "./assets/main.css";
 
 // TODO: 1. Installiere Pinia über einen Package Manager ("npm install pinia")
-// TODO: 3. Erstelle eine neue Pinia-Instanz und binde sie an Vue
+// TODO: 3. Erstelle eine neue Pinia-Instanz und installiere sie in Vue
 createApp(App).mount("#app");
