0601ce6fcafff07ac9da70de7cace2132f25134d
eld <elias.dolinsek@gmail.com>
2023-06-23 14:12:57 +0200

    Finished

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 
 import "./assets/main.css";
 
-createApp(App).mount("#app");
+const pinia = createPinia();
+createApp(App).use(pinia).mount("#app");
